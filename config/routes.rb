8cd3ac73879ba70255f7806e507d30cfff3092e8
ajmal rambocus <ajmalrambocus@gmail.com>
2024-08-27 13:10:11 +0400

    all done

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  # index
+  # get "restaurants", to: "restaurants#index"
+  # # new
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # #create
+  # post "restaurants", to: "restaurants#create"
+  # # show
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # # edit
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # # update
+  # patch "restaurants/:id", to: "restaurants#update"
+  # # delete
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  #
+
+  resources :restaurants
+
+end
